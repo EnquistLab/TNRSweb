1b124510961f19c407a020b971353abb1e9f75d3
rethvick <furyassassinofficial@gmail.com>
2024-06-11 13:55:48 -0700

    Code Update

--- next.config.js
+++ next.config.js
@@ -2,15 +2,17 @@ module.exports = {
   trailingSlash: true,
   env: {
     // development
-   //apiServer: "http://vegbiendev.nceas.ucsb.edu:8975/",
+   // apiServer: "http://vegbiendev.nceas.ucsb.edu:9975/",
     // production
-    // apiServer: "https://tnrsapi.xyz/",
-    apiServer: "http://tnrsapidev.xyz/tnrs_api.php",
-    // development
-    // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:9975/tnrs_api.php",
-    // production
-    apiEndPoint: "http://tnrsapidev.xyz/tnrs_api.php",
-    //apiEndPoint: "https://tnrsapi.xyz/tnrs_api.php",
+     apiServer: "https://tnrsapi.xyz/",
+   // apiServer: "http://tnrsapidev.xyz/tnrs_api.php",
+    // development (Private)
+    //  apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:8975/tnrs_api.php",
+      // development (Public)
+      // apiEndPoint: "http://vegbiendev.nceas.ucsb.edu:9975/tnrs_api.php",
+      // production
+    // apiEndPoint: "http://tnrsapidev.xyz/tnrs_api.php",
+     apiEndPoint: "https://tnrsapi.xyz/tnrs_api.php",
     defaultMatchingThreshold: .53
   },
 };
